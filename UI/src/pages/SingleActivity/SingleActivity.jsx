bf5a3ccf8774003bc84f857cbbf16cc80ed9ce4b
NikhilSharma03 <nikhilsharmamusic2000@gmail.com>
2023-12-10 06:44:57 +0530

    feat: update single activity page

--- UI/src/pages/SingleActivity/SingleActivity.jsx
+++ UI/src/pages/SingleActivity/SingleActivity.jsx
@@ -1,9 +1,17 @@
 import './SingleActivity.css'
+import { useParams } from 'react-router-dom'
 
 const SingleActivity = () => {
+  const { id } = useParams()
+
   return (
     <div className='single__activity--container'>
-      <h1 className='single__activity__header'>Activity 1</h1>
+      {id === '1' && (
+        <h1 className='single__activity__header'>Stable Intent Reward</h1>
+      )}
+      {id === '2' && (
+        <h1 className='single__activity__header'>High Risk Leveraged Reward</h1>
+      )}
       <div className='flex '>
         <button className='btn btn-primary'>Withdraw</button>
       </div>
